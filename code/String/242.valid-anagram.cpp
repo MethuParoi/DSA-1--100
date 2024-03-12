a43e3ea717a65305a13d97f8633115adfcf05c60
MethuParoi <paroimethu@gmail.com>
2024-03-12 12:26:24 +0530

    242-valid-anagram-solved_12-march-24

---
+++ code/String/242.valid-anagram.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+//Valid Anagram
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        cout<<s<<endl;
+        cout<<t;
+
+        if(s == t){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
